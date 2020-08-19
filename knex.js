6a5546b570a4477d82ae7cedcc9c6671b47f0db2
Omodot_E <omodote@hotmail.com>
2020-08-19 19:47:31 +0100

    update knex connection

--- knex.js
+++ knex.js
@@ -5,7 +5,8 @@ const knex = require("knex");
 //"postgress://postgres:{password}@localhost:{port_number}/{db_name}"
 const db = new knex({
   client: "pg",
-  connection: "postgres://postgres:omodot@localhost:5432/pacerhr",
+  connection: process.env.DATABASE_URL,
+  // connection: "postgres://postgres:omodot@localhost:5432/pacerhr",
 });
 
 module.exports = db;
